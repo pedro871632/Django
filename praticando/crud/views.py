3ed5885b8134fe679550e113442ee76d99dcd918
Pedro Lucas Andrade <pedroluxa871632@gmail.com>
2023-09-04 15:49:38 -0300

    Adicionando rota /register para fazer uma request e enviar uma resposta(teste)

---
+++ praticando/crud/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+
+def register(request):
+
+
+    return HttpResponse("Cliente cadastrado com sucesso !\n Apenas um teste, boy") 
+
+
+
+# Create your views here.
